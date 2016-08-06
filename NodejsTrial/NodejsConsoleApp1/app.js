6aa087fc71d0e38fedba474a6a02dde55da1d3cd
HK Zhang <yxzhk@hotmail.com>
2016-08-06 10:18:10 +0800

    NodeJsTrial

---
+++ NodejsTrial/NodejsConsoleApp1/app.js
@@ -0,0 +1,16 @@
+﻿var sys = require("sys");
+
+setTimeout(function () { sys.puts("world"); }, 3000);
+sys.puts("hello");
+
+var http = require("http");
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(5000, "127.0.0.1");
+
+var net = require("net");
+net.createServer(function (socket) {
+    socket.write("Echo Server\r\n");
+    socket.pipe(socket);
+}).listen(6000, "127.0.0.1");
