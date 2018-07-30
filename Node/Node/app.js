5f7137ba05fe45f28be2acb1a3b037e1d78bdf46
yxzhk <yxzhk@hotmail.com>
2018-07-30 21:27:44 +0800

    enable import and export on nodejs

--- Node/Node/app.js
+++ Node/Node/app.js
@@ -1,6 +1,9 @@
 'use strict';
+import { square, diag } from './lib';
 const nodeEvent = require('./nodeEvent');
 // import simple_timer_demo from './nodeEvent/simple_timer'
 
 nodeEvent.simple_timer();
 // console.log('Hello world');
+console.log(square(11)); // 121
+console.log(diag(4, 3)); // 5
