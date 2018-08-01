93f31cf228777d965fd03bd8933000e4ceb3d8e6
yxzhk <yxzhk@hotmail.com>
2018-08-01 21:10:45 +0800

    settimeout and setimmediate

--- Node/Node/app.js
+++ Node/Node/app.js
@@ -3,7 +3,10 @@ import { square, diag } from './lib';
 const nodeEvent = require('./nodeEvent');
 // import simple_timer_demo from './nodeEvent/simple_timer'
 
-nodeEvent.simple_timer();
-// console.log('Hello world');
-console.log(square(11)); // 121
-console.log(diag(4, 3)); // 5
+// console.log(square(11)); // 121
+// console.log(diag(4, 3)); // 5
+
+// nodeEvent.simple_timer();
+// nodeEvent.simple_interval();
+// nodeEvent.nexttick_demo();
+nodeEvent.timeout_immediate();
--- Node/Node/nodeEvent.js
+++ Node/Node/nodeEvent.js
@@ -15,3 +15,84 @@ exports.simple_timer = () => {
     console.time("50MilliSecond");
     setTimeout(simpleTimeout, 50, "50MilliSecond");
 }
+
+exports.simple_interval = () => {
+    var x = 0, y = 0, z = 0;
+    function displayValues(){
+        console.log("X=%d; Y=%d; Z=%d", x, y, z);
+    }
+
+    function updateX(){
+        x += 1;
+    }
+
+    function updateY(){
+        y += 1;
+    }
+
+    function updateZ(){
+        z += 1;
+        displayValues();
+    }
+
+    setInterval(updateX, 500);
+    setInterval(updateY, 1000);
+    setInterval(updateZ, 2000);
+}
+
+exports.nexttick_demo = () => {
+    var fs = require("fs");
+    fs.stat("nodeEvent.js", function (err, stats) {
+        if (stats) { console.log("nodeEvent.js Exists"); }
+    });
+
+    setTimeout(function () {
+        console.log("Timeout Timer 1 Executed");
+    },0);
+
+    setImmediate(function () {
+        console.log("Immediate Timer 1 Executed");
+    });
+
+    setTimeout(function () {
+        console.log("Timeout Timer 2 Executed");
+    },0);
+
+    setImmediate(function () {
+        console.log("Immediate Timer 2 Executed");
+    });
+
+    process.nextTick(function () {
+        console.log("Next Tick 1 Executed");
+    });
+
+    process.nextTick(function () {
+        console.log("Next Tick 2 Executed");
+    });
+}
+
+exports.timeout_immediate = () => {
+    var fs = require("fs");
+    fs.stat("nodeEvent.js", function (err, stats) {
+        setImmediate(() => console.log("IO_Callback_Immediate"));
+        setTimeout(() => console.log("IO_Callback_Timeout"), 0);
+    });
+
+    setTimeout(()=>{
+        setImmediate(() => console.log("Timer_Callback_Immediate"));
+        setTimeout(() => console.log("Timer_Callback_Timeout"), 0);
+    }, 0)
+
+    setImmediate(()=>{
+        setImmediate(() => console.log("Checker_Callback_Immediate"));
+        setTimeout(() => console.log("Checker_Callback_Timeout"), 0);
+    })
+
+    // Output:
+    // Timer_Callback_Immediate
+    // Timer_Callback_Timeout
+    // Checker_Callback_Timeout
+    // Checker_Callback_Immediate
+    // IO_Callback_Immediate
+    // IO_Callback_Timeout
+}
