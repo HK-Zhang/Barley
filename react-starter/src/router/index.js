064708e50baac709e4160be105e6b02028b84a65
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2018-08-16 17:16:54 +0800

    route children

--- react-starter/src/router/index.js
+++ react-starter/src/router/index.js
@@ -1,3 +1,4 @@
 export {default as BasicExample} from './BasicExample';
 export {default as ParamsExample} from './ParamsExample';
 export {default as AuthExample} from './AuthExample';
+export {default as CustomLinkExample} from './CustomLinkExample';
