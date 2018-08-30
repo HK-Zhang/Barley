1a4837419c400c00b5bd8e951680570bc3cea115
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2018-08-30 17:17:56 +0800

    root with animation

--- react-starter/src/router/index.js
+++ react-starter/src/router/index.js
@@ -3,3 +3,8 @@ export {default as ParamsExample} from './ParamsExample';
 export {default as AuthExample} from './AuthExample';
 export {default as CustomLinkExample} from './CustomLinkExample';
 export {default as PreventingTransitionsExample} from './PreventingTransitionsExample';
+export {default as AnimationExample} from './AnimationExample';
+export {default as SidebarExample} from './SidebarExample';
+export {default as RecursiveExample} from './RecursiveExample';
+export {default as NoMatchExample} from './NoMatchExample';
+
