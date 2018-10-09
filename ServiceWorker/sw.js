865079d0d2e38fac8180a4b5cfaaa03a635b9f52
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2018-10-09 13:11:34 +0800

    skip waiting and manual updates

--- ServiceWorker/sw.js
+++ ServiceWorker/sw.js
@@ -1,24 +1,64 @@
+// self.addEventListener('install', event => {
+//     console.log('V1 installing…');
+//     // cache a cat SVG
+//     event.waitUntil(
+//         caches.open('static-v1').then(cache => cache.add('cat.svg'))
+//     );
+// });
+
+// self.addEventListener('activate', event => {
+//     // You can take control of uncontrolled clients by calling clients.claim() within your service worker once it's activated.
+//     // You should see a cat the first time. 
+//     // clients.claim();
+//     console.log('V1 now ready to handle fetches!');
+// });
+
+// self.addEventListener('fetch', event => {
+//     const url = new URL(event.request.url);
+
+//     // serve the cat SVG from the cache if the request is
+//     // same-origin and the path is '/dog.svg'
+//     if (url.origin == location.origin && url.pathname.endsWith('/dog.svg')) {
+//         event.respondWith(caches.match('cat.svg'));
+//     }
+// });
+
+// experiment two: commnet above code and uncomment below code.
+const expectedCaches = ['static-v2'];
+
 self.addEventListener('install', event => {
-    console.log('V1 installing…');
-    // cache a cat SVG
+    console.log('V2 installing…');
+
+    //  you can make your new service worker activate sooner by calling self.skipWaiting().
+    self.skipWaiting();
+    // cache a horse SVG into a new cache, static-v2
     event.waitUntil(
-        caches.open('static-v1').then(cache => cache.add('cat.svg'))
+        caches.open('static-v2').then(cache => cache.add('/horse.svg'))
     );
 });
 
 self.addEventListener('activate', event => {
-    // You can take control of uncontrolled clients by calling clients.claim() within your service worker once it's activated.
-    // You should see a cat the first time. 
-    // clients.claim();
-    console.log('V1 now ready to handle fetches!');
+    // delete any caches that aren't in expectedCaches
+    // which will get rid of static-v1
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (!expectedCaches.includes(key)) {
+                    return caches.delete(key);
+                }
+            })
+        )).then(() => {
+            console.log('V2 now ready to handle fetches!');
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
 
-    // serve the cat SVG from the cache if the request is
+    // serve the horse SVG from the cache if the request is
     // same-origin and the path is '/dog.svg'
-    if (url.origin == location.origin && url.pathname.endsWith('/dog.svg')) {
-        event.respondWith(caches.match('cat.svg'));
+    if (url.origin == location.origin && url.pathname == '/dog.svg') {
+        event.respondWith(caches.match('/horse.svg'));
     }
 });
