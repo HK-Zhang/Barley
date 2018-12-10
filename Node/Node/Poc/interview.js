82837cff1ac381caf05563864b6e901c4e05c815
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2018-12-10 13:01:30 +0800

    interveiw callback

--- Node/Node/Poc/interview.js
+++ Node/Node/Poc/interview.js
@@ -44,8 +44,51 @@ TestB = () => {
     // false
     console.log(0.2 + 0.1 === 0.3);
     // false
-    console.log((0.2*10 + 0.1*10)/10);
+    console.log((0.2 * 10 + 0.1 * 10) / 10);
     console.log(0.2 + 0.1);
 }
 
-TestB();
+TestCallback = () => {
+    var http = require("http");
+
+    http.request("http://localhost:3000/students", (response) => {
+        response.on('data', (data) => {
+            students = JSON.parse(data);
+            result = [];
+            students.forEach(s => {
+                result.push({ "id": s.id, "name": s.name, "average": 0 });
+                http.request("http://localhost:3000/courses?studentId=" + s.id, (response) => {
+                    response.on('data', (data) => {
+                        course = JSON.parse(data);
+                        s.courseCount = course.length;
+                        s.totalScores = 0;
+                        course.forEach(c => {
+                            http.request("http://localhost:3000/" + c.id + "?id=" + s.id, (response) => {
+                                response.on('data', (data) => {
+                                    score = JSON.parse(data);
+                                    score.forEach(sc => {
+                                        s.totalScores += sc.score;
+                                    });
+                                });
+
+                                response.on('end', () => {
+                                    if (course[course.length - 1] == c && students[students.length - 1] == s) {
+                                        result.forEach(r => {
+                                            std = students.find(std => std.id == r.id);
+                                            r.average = std.totalScores / std.courseCount;
+                                        });
+                                        console.log(JSON.stringify(students));
+                                        console.log(JSON.stringify(result));
+                                    }
+                                });
+                            }).end();
+                        });
+                    });
+                }).end();
+            });
+
+        });
+    }).end();
+}
+
+TestCallback();
