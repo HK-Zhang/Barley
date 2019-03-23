0632fb3137c87bfe1050c39ddecf6b57381a1400
yxzhk <yxzhk@hotmail.com>
2019-03-23 13:05:04 +0800

    reorg demo with onedesign

--- ant-app/src/containers/alertDemo.jsx
+++ ant-app/src/containers/alertDemo.jsx
@@ -1,5 +1,6 @@
 import { Alert } from 'antd';
 import React from 'react';
+import styles from './alertDemo.module.scss';
 
 const onClose = (e) => {
     console.log(e, 'I was closed.');
@@ -7,25 +8,34 @@ const onClose = (e) => {
 
 
 const BasicAlert = () => (
-    <div>
-        <Alert message="Success Text" type="success" />
-        <Alert message="Info Text" type="info" />
-        <Alert message="Warning Text" type="warning" />
-        <Alert message="Error Text" type="error" />
-        <Alert
-            message="Warning Text Warning Text Warning TextW arning Text Warning Text Warning TextWarning Text"
-            type="warning"
-            closable
-            onClose={onClose}
-        />
-        <Alert
-            message="Error Text"
-            description="Error Description Error Description Error Description Error Description Error Description Error Description"
-            type="error"
-            closable
-            onClose={onClose}
-        />
-    </div>
+    <React.Fragment>
+        <div className={styles.divframe}>
+        <h2>Alert</h2>
+            <ul>
+                <li><Alert message="Success Text" type="success" /></li>
+                <li><Alert message="Info Text" type="info" /></li>
+                <li><Alert message="Warning Text" type="warning" /></li>
+                <li><Alert message="Error Text" type="error" /></li>
+            </ul>
+        </div>
+        <div className={styles.divframe}>
+        <h2>Alert Close</h2>
+            <ul><li><Alert
+                message="Warning Text Warning Text Warning TextW arning Text Warning Text Warning TextWarning Text"
+                type="warning"
+                closable
+                onClose={onClose}
+            /></li>
+                <li><Alert
+                    message="Error Text"
+                    description="Error Description Error Description Error Description Error Description Error Description Error Description"
+                    type="error"
+                    closable
+                    onClose={onClose}
+                /></li>
+            </ul>
+        </div>
+    </React.Fragment>
     );
 
 
--- ant-app/src/containers/formDemo.jsx
+++ ant-app/src/containers/formDemo.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
     Form, Icon, Input, Button,
   } from 'antd';
+  import styles from './formDemo.module.scss';
   
   function hasErrors(fieldsError) {
     return Object.keys(fieldsError).some(field => fieldsError[field]);
@@ -31,37 +32,38 @@ import {
       const userNameError = isFieldTouched('userName') && getFieldError('userName');
       const passwordError = isFieldTouched('password') && getFieldError('password');
       return (
-        <Form layout="inline" onSubmit={this.handleSubmit}>
-          <Form.Item
-            validateStatus={userNameError ? 'error' : ''}
-            help={userNameError || ''}
-          >
-            {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-            })(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-            )}
-          </Form.Item>
-          <Form.Item
-            validateStatus={passwordError ? 'error' : ''}
-            help={passwordError || ''}
-          >
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={hasErrors(getFieldsError())}
+        <div className={styles.divframe}>
+          <Form layout="inline" onSubmit={this.handleSubmit}>
+            <Form.Item
+              validateStatus={userNameError ? 'error' : ''}
+              help={userNameError || ''}
             >
-              Log in
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              )}
+            </Form.Item>
+            <Form.Item
+              validateStatus={passwordError ? 'error' : ''}
+              help={passwordError || ''}
+            >
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              )}
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={hasErrors(getFieldsError())}
+              >
+                Log in
             </Button>
-          </Form.Item>
-        </Form>
+            </Form.Item>
+          </Form></div>
       );
     }
   }
