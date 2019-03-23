1ac627692d2eb91404f14a8a19d16840d4b21e7f
yxzhk <yxzhk@hotmail.com>
2019-03-23 08:57:28 +0800

    adjust style

--- ant-app/src/containers/inputDemo.jsx
+++ ant-app/src/containers/inputDemo.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Input } from 'antd';
+import styles from './inputDemo.module.scss';
 
 const BasicInput = () => (
-	<Input placeholder="Basic usage" />
+	<div className={styles.divframe}>
+	<h2>Input Demo</h2>
+		<ul>
+			<li><Input placeholder="Basic usage one" /></li>
+			<li><Input placeholder="Basic usage two " /></li>
+			<li><Input placeholder="Basic usage three" /></li>
+		</ul>
+	</div>
 );
 
 
