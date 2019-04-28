4dcf8bcdccf1f7d9bfc7e4f09d72a017402adca0
yxzhk <yxzhk@hotmail.com>
2019-04-28 21:18:14 +0800

    filter and sorter table

--- ant-app/src/containers/tableDemo.jsx
+++ ant-app/src/containers/tableDemo.jsx
@@ -1,8 +1,11 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import styles from './tableDemo.module.scss';
 import BasicTable from "../components/table/basicTable";
 import JsxStyleTable from '../components/table/jsxStyleTable';
 import SelectionTable from '../components/table/selectionTable';
+import SelOpsTable from '../components/table/selopsTable';
+import CustomSelectionTable from '../components/table/customSelTable';
+import FilterSorterTable from '../components/table/filterSorterTable';
 
 const TableDemo = () => (
     <React.Fragment>
@@ -17,6 +20,18 @@ const TableDemo = () => (
         <div className={styles.divframe}>
             <h2>selection</h2>
             <SelectionTable></SelectionTable>
+        </div>
+        <div className={styles.divframe}>
+            <h2>Selection and operation</h2>
+            <SelOpsTable></SelOpsTable>
+        </div>
+        <div className={styles.divframe}>
+            <h2>Custom selection</h2>
+            <CustomSelectionTable></CustomSelectionTable>
+        </div>
+        <div className={styles.divframe}>
+            <h2>Reset filters and sorters</h2>
+            <FilterSorterTable></FilterSorterTable>
         </div>
     </React.Fragment>
 )
