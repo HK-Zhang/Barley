5dcc824117efc8f978ea5aaefdb89933b13450a6
yxzhk <yxzhk@hotmail.com>
2019-06-13 20:33:04 +0800

    upload

--- ant-app/src/containers/uploadDemo.jsx
+++ ant-app/src/containers/uploadDemo.jsx
@@ -6,6 +6,9 @@ import Avatar from '../components/upload/avatarUpload';
 import DefaultFileUpload from '../components/upload/defaultFileUplad';
 import PicturesWall from '../components/upload/pictureWallUpload';
 import DirectoryUpload from '../components/upload/directoryUpload';
+import FileListUpload from '../components/upload/fileListUpload';
+import ManuallyUpload from '../components/upload/ManuallyUpload';
+import PictureListUpload from '../components/upload/pictureListUpload';
 
 const UploadDemo = () => (
     <React.Fragment>
@@ -32,6 +35,18 @@ const UploadDemo = () => (
         <div className={styles.divframe}>
             <h2>Upload Directory</h2>
             <DirectoryUpload></DirectoryUpload>
+        </div>
+        <div className={styles.divframe}>
+            <h2>Complete control over file list</h2>
+            <FileListUpload></FileListUpload>
+        </div>
+        <div className={styles.divframe}>
+            <h2>Manually Upload</h2>
+            <ManuallyUpload></ManuallyUpload>
+        </div>
+        <div className={styles.divframe}>
+            <h2>Pictures with list style</h2>
+            <PictureListUpload></PictureListUpload>
         </div>
     </React.Fragment>
 )
