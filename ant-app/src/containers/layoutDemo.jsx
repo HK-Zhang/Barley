017e441dbd7c3c6f73e1cc1e9acf28f872164971
yxzhk <yxzhk@hotmail.com>
2019-06-25 21:09:36 +0800

    layout

--- ant-app/src/containers/layoutDemo.jsx
+++ ant-app/src/containers/layoutDemo.jsx
@@ -4,6 +4,8 @@ import styles from './layoutDemo.module.scss';
 import BasicLayout from '../components/layout/basicLayout';
 import HeaderContentFooter from '../components/layout/headerContentFooter';
 import HeaderSider from '../components/layout/headerSider';
+import HeaderSider2 from '../components/layout/headerSider2';
+
 
 const LayoutDemo = () => (
     <React.Fragment>
@@ -18,6 +20,10 @@ const LayoutDemo = () => (
         <div className={styles.divframe}>
             <h2>Header sider</h2>
             <HeaderSider></HeaderSider>
+        </div>
+        <div className={styles.divframe}>
+            <h2>Header sider2</h2>
+            <HeaderSider2></HeaderSider2>
         </div>
     </React.Fragment>
 )
